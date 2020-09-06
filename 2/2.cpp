58b09b5d0ca4039019366b1014eb6f40c83ed2af
Andy Simphaly <andytsimphaly@gmail.com>
2020-09-05 23:07:49 -0500

    Completed problem 2 by using Binet's Formula and summation

--- 2/2.cpp
+++ 2/2.cpp
@@ -5,46 +5,37 @@
 #include <math.h>
 using namespace std;
 
-//Global Variable for Binet's Formula
+//Global Variables for Binet's Formula
 const float SQRT_FIVE = pow(5, .5);
 const float PHI = (1 + SQRT_FIVE) / 2;
 const int LIMIT = 4000000;
 
-//Description:
-//Precondition:
-//Postcondition:
-int evenfib(int n);
-
+//Function for calculating the sum of even fibonacci numbers up to the lim.
+int evenfibsum(int lim);
 
 int main(){
 
   //Variables
   int sum = 0;
-  int i = 2;
 
   //Function call
-  // do {
-  //   cout << sum << endl;
-  //   sum += evenfib(i);
-  //   i++;
-  // } while (evenfib(i-1) <= LIMIT);
+  sum = evenfibsum(LIMIT);
 
   //Output the sum
-  int temp = floor(log(LIMIT * SQRT_FIVE) / log(PHI));
-  int n = floor(temp / 3);
-  int ans = ((pow(PHI, (3 * n + 3)) - 1) / (pow(PHI , 3) - 1)
-    - (pow((1 - PHI), (3 * n + 3)) - 1) / (pow((1 - PHI) , 3) - 1)) / SQRT_FIVE;
-  cout << temp << " " << n << " " << ans << endl;
   cout << sum << endl;
 
   return 0;
 };
 
 //Function for finding even fibonacci values
-int evenfib(int n) {
-  int num = (1/SQRT_FIVE) * (pow(PHI, n) - pow( - pow(PHI, -1), n));
-  if (num % 2 == 0) {
-    return num;
-  }
-  return 0;
+int evenfibsum(int lim) {
+  //Fibonacci numbers are only even at every third interval
+  int temp = floor(log(lim * SQRT_FIVE) / log(PHI));
+  int n = floor(temp / 3);
+
+  //Using sumnation to solve
+  int ans = ((pow(PHI, (3 * n + 3)) - 1) / (pow(PHI , 3) - 1)
+    - (pow((-1 / PHI), (3 * n + 3)) - 1) / (pow((-1 / PHI) , 3) - 1)) / SQRT_FIVE;
+
+  return ans;
 }
