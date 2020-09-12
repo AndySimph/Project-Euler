18289d09065ef77b943d970363c71e775781d803
Andy Simphaly <andytsimphaly@gmail.com>
2020-09-12 04:42:55 -0500

    Completed problem 4 and added comments

--- 4/4.cpp
+++ 4/4.cpp
@@ -4,30 +4,52 @@
 #include <iostream>
 using namespace std;
 
+//Function to check if integer is a palindrome
+bool palCheck(int num);
+
 int main(){
 
   //Variables
-  bool isPal = false;
+  int pal = 0;
+  int n1 = 999;
+  int n2 = 999;
+  int i=0;
+  int t;
 
-  int num = 9009;
-  int rev = 0;
-  int val = num;
-
-   while(num > 0) {
-      rev = rev * 10 + num % 10;
-      num = num / 10;
-   }
-   if (val == rev) {
-      isPal = true;
-   }
-
-  //Output the sum
-  if (isPal) {
-    cout << num << endl;
-  } else {
-    cout << "Nop" <<endl;
+  //While loop to check through integers
+  while (!pal) {
+      for(int j=0;j<2;j++) {
+          for(int k=0; k<=i; k++) {
+              //Go through various numbers
+              t=(n1-i-k)*(n2-i+k);
+              if(palCheck(t))
+                  pal = t;
+          }
+          ++i;
+      }
   }
 
+  //Output
+  cout << pal << endl;
 
   return 0;
 };
+
+//palCheck Function
+bool palCheck (int num) {
+  //Variables
+  int rev = 0;
+  int val = num;
+
+  //While loop to check through each digit and insert it into reversed var
+  while(num > 0) {
+    rev = rev * 10 + num % 10;
+    num = num / 10;
+  }
+
+  //Return true if palindrome
+  if (val == rev) {
+    return true;
+  }
+  return false;
+}
