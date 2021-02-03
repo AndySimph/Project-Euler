3580cddb6cd9591a22a8a5ec8d276575aed5ba76
Andy Simphaly <andytsimphaly@gmail.com>
2021-02-03 01:34:39 -0600

    Added algorithm to search and compute product throughtout the matrix, and completed the problem

--- 11/11.cpp
+++ 11/11.cpp
@@ -34,14 +34,47 @@ int main () {
         j++;
     }
 
-    //For loop to cout matrix to terminal
+    //Variable for the product and variable for a temporary value
+    int product = 0;
+    int temp_val = 0;
+
+    //For loop to check each set of four
     for(int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            std::cout << matrix[i][j] << " ";
+            //Checks if it is able to go diagonally down and right, get the product and check it with the current product
+            if ((j > 4) && (i < 17)) {
+                temp_val = std::stoi(matrix[i][j]) * std::stoi(matrix[i+1][j-1]) * std::stoi(matrix[i+2][j-2]) * std::stoi(matrix[i+3][j-3]);
+                if (temp_val >= product) {
+                    product = temp_val;
+                }
+            }
+            //Checks if it can go down 3, get the product and check it with the current product
+            if (i < 17) {
+                temp_val = std::stoi(matrix[i][j]) * std::stoi(matrix[i+1][j]) * std::stoi(matrix[i+2][j]) * std::stoi(matrix[i+3][j]);
+                if (temp_val >= product) {
+                    product = temp_val;
+                }
+            }
+            //Checks if it can go to the right 3, get the product and check it with the current product
+            if (j < 17) {
+                temp_val = std::stoi(matrix[i][j]) * std::stoi(matrix[i][j+1]) * std::stoi(matrix[i][j+2]) * std::stoi(matrix[i][j+3]);
+                if (temp_val >= product) {
+                    product = temp_val;
+                }
+            }
+            //Checks if it can go diagonally right, get the product and check it with the current product
+            if ((j < 17) && (i < 17)) {
+                temp_val = std::stoi(matrix[i][j]) * std::stoi(matrix[i+1][j+1]) * std::stoi(matrix[i+2][j+2]) * std::stoi(matrix[i+3][j+3]);
+                if (temp_val >= product) {
+                    product = temp_val;
+                }
+            }
             
         }
-        std::cout << std::endl;
     }
+
+    //Output the product
+    std::cout << product << std::endl;
 
     return 0;
 } 
