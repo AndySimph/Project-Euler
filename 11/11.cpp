fef51a230f85108017eedc41466171284c7b0a6e
Andy Simphaly <andytsimphaly@gmail.com>
2021-02-02 21:08:35 -0600

    Created the matrix text file and method of getting the given matrix into a 2d array

--- 11/11.cpp
+++ 11/11.cpp
@@ -1,7 +1,47 @@
 //Andy Simphaly
 
+//Libraries
+#include <iostream>
+#include <fstream>
+#include <string>
+
 int main () {
 
+    const int SIZE = 20;
+
+    std::string matrix[SIZE][SIZE];
+
+    //Open the file with the given matrix values
+    std::ifstream file;
+    file.open ("matrix.txt");
+
+    //Variables for the matrix position
+    int i = 0;
+    int j = 0;
+
+    //String to take in each number
+    std::string num;
+    while (file >> num)
+    {
+        //Set each positino accordingly
+        matrix[i][j] = num;
+
+        //Increment accordingly
+        if (j == SIZE) {
+            j = 0;
+            i++;
+        }   
+        j++;
+    }
+
+    //For loop to cout matrix to terminal
+    for(int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            std::cout << matrix[i][j] << " ";
+            
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
-}
+} 
