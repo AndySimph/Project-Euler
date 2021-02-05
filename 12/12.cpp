ea61922416c8b9cc9839c3da7b3f7fa24a87abb8
Andy Simphaly <andytsimphaly@gmail.com>
2021-02-04 23:00:38 -0600

    Added some parts to my algorithm for problem 12

--- 12/12.cpp
+++ 12/12.cpp
@@ -2,8 +2,30 @@
 
 //Libraries
 #include <iostream>
+#include <cmath>
 
 int main() {
+
+    bool quit = false;
+    int triangle = 0;
+
+    while (!(quit)) {
+        
+        long sum = 0;
+        for (int i = triangle; i >= 0; i--) {
+            sum += i;
+        }
+
+        
+        triangle++;
+
+        if (triangle == 5) {
+            quit = true;
+        }
+
+    }
+
+    std::cout << triangle << std::endl;
 
     return 0;
 }
