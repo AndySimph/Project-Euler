1a78db40b05ec95be09e1a8a25c8e7486fe1f228
Andy Simphaly <andytsimphaly@gmail.com>
2021-02-04 23:03:14 -0600

    Added comments

--- 12/12.cpp
+++ 12/12.cpp
@@ -6,25 +6,34 @@
 
 int main() {
 
+    //Variables
     bool quit = false;
     int triangle = 0;
+    long sum;
+    int num_factors;
 
+    //Loop to continue until told to stop
     while (!(quit)) {
         
-        long sum = 0;
+        //Variables that need to be reset for every triangle
+        sum = 0;
+        num_factors = 0;
+
+        //Add every natural number of the triangle
         for (int i = triangle; i >= 0; i--) {
             sum += i;
         }
 
-        
+        //Increment the triangle
         triangle++;
 
+        //If statement to quit the loop
         if (triangle == 5) {
             quit = true;
         }
-
     }
 
+    //Output
     std::cout << triangle << std::endl;
 
     return 0;
