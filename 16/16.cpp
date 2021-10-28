000b94f64984d16586d787d3689c5c96d0b132c8
Andy Simphaly <andytsimphaly@gmail.com>
2021-10-28 17:16:17 -0500

    Added some default testing values

--- 16/16.cpp
+++ 16/16.cpp
@@ -3,8 +3,14 @@
 //Libraries
 #include <iostream>
 #include <vector>
+#include <tgmath.h>
 
 int main () {
+    //Variables
+    int base = 2;
+    int power = 15;
+
+    std::cout << pow(base, power) << std::endl;
 
     return 0;
 }
