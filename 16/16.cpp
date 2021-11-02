2fb26b682a5aafd4a0ff3256ef3290c47e3bf41d
Andy Simphaly <andytsimphaly@gmail.com>
2021-11-01 19:20:55 -0500

    Added comments

--- 16/16.cpp
+++ 16/16.cpp
@@ -10,20 +10,25 @@ int main () {
     int base = 2;
     int power = 1000;
     int result = pow(base, power);
-
     int length = 0;
+    int sum = 0;
+
+    //Temporary assignment
     int temp_var = result;
 
-    while (temp_var != 0)
-    {
+    //Find length of the number
+    while (temp_var != 0) {
         temp_var = temp_var / 10;
         ++length;
     }
 
+    //Re assign the temp var
     temp_var = result;
 
+    //Define the array
     int array [length];
 
+    //Loop through and assign the array
     for (int i = 0; i < length; i++) {
 
         array[i] = temp_var % 10;
@@ -31,14 +36,13 @@ int main () {
 
     }
 
-    int sum = 0;
-
+    //Calculate the sum
     for (int i = 0; i < length; i++) {
         sum += array[i];
     }
 
+    //Output
     std::cout << sum << std::endl;
 
-    
     return 0;
 }
