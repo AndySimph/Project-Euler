847174d17740ad273bfb109c1586ce2b0b261cac
Andy Simphaly <andytsimphaly@gmail.com>
2022-02-27 22:04:57 -0600

    Implemented two ways to solve this problem

--- 15/15.cpp
+++ 15/15.cpp
@@ -1,6 +1,6 @@
 //Andy Simphaly
 //File:	    15.cpp
-//Purpose:	Driver function
+//Purpose:	Driver file
 
 //Libraries
 #include <iostream>
@@ -9,10 +9,41 @@
 
 int main () {
 
-    //Matrix of the entire block
-    int matrix[21][21];
+    long paths = 1;
 
-    std::cout << "Here" << std::endl;
+    for (int i = 0; i < max_size; i ++) {
+        paths *= (2 * max_size) - i;
+        paths /= i + 1;
+    }
 
+    std::cout << paths << std::endl;
+
+    long grid[max_size + 1][max_size + 1];
+
+    for (int i = 0; i < max_size; i++) {
+        for (int j = 0; j < max_size; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < max_size; i++) {
+        // std::cout << grid[i][max_size];
+        grid[i][max_size] = 1;
+        grid[max_size][i] = 1;
+    }
+
+    for (int i = max_size - 1; i >= 0; i--) {
+        for (int j = max_size - 1; j >= 0; j--) {
+            grid[i][j] = grid[i+1][j] + grid[i][j+1];
+        }
+    }
+
+    for (int i = 0; i < max_size; i++) {
+        for (int j = 0; j < max_size; j++) {
+            std::cout << grid[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    
     return 0;
 }
